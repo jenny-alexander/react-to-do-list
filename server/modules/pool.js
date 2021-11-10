d0486894ad11c57682e8554c2058a434832502a9
Jennifer Alexander <jenny_alexander@icloud.com>
2021-11-10 12:26:28 -0600

    Add feature to deploy to Heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,24 @@
 const pg = require( 'pg' );
+require('dotenv').config();
 
-const pool = new pg.Pool(
-    {
-        database: 'weekend_to_do_app',
-        localhost: 'localhost',
-        port: 5432,
-        max: 12,
-        idleTimeoutMillis: 30000
-    }
-)
+//Depending on where the app is deployed (heroku or local), get the db connection requirements
+const pool = ( () => {
+    if ( process.env.NODE_ENV !== 'production' ) {
+        return new pg.Pool({
+            database: 'weekend_to_do_app',
+            host: 'localhost',
+            port: 5432,
+            max: 12,
+            idleTimeoutMillis: 30000
+        } );
+    } else {
+        return new pg.Pool( {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+              }
+        } );
+    } } ) ();
+//NEW END
 
 module.exports = pool;
