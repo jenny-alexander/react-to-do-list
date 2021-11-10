890aa4578987738b4ade6c654d62dd2f00d1d195
Jennifer Alexander <jenny_alexander@icloud.com>
2021-11-10 13:23:52 -0600

    Trying another way to get Heroku to work

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use( '/todo', todo );
 
 //Use this for heroku
 if ( process.env.NODE_ENV === 'production' ) {
-    app.use( express.static( 'build' ) );
+    app.use( express.static( '../build' ) );
 
     app.get('*', ( req, res )=> {
         res.sendFile( path.join(__dirname, 'build', 'index.html' ) ) ; //relative path
