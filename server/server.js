f1bd95185be6dfc47c523ff16df7de22bf5edf63
Jennifer Alexander <jenny_alexander@icloud.com>
2021-11-10 13:09:35 -0600

    Add changes for app to work on heroku

--- server/server.js
+++ server/server.js
@@ -3,16 +3,26 @@ const express = require( 'express' );
 const app = express();
 const todo = require( './routes/todo.route' );
 const bodyParser = require( 'body-parser' );
+const path = require( 'path' );
 
 //uses
 app.use( bodyParser.urlencoded( { extended:true } ) );
 app.use( bodyParser.json() );
 app.use( '/todo', todo );
 
+//Use this for heroku
+if ( process.env.NODE_ENV === 'production' ) {
+    app.use( express.static( 'build' ) );
+
+    app.get('*', ( req, res )=> {
+        res.sendFile( path.join(__dirname, 'build', 'index.html' ) ) ; //relative path
+    })
+}
+
 //global
-const port = 5001;
+const PORT = process.env.PORT || 5001;
 
 //spin up server
-app.listen( port, ()=>{
+app.listen( PORT, ()=>{
     console.log( `server up on:`, port );
 });
